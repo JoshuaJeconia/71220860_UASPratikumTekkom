915f057a6249e11b6b56bad8d895f3350113609b
JoshuaJeconia <117901765+JoshuaJeconia@users.noreply.github.com>
2023-01-03 13:49:48 +0700

    Add files via upload

---
+++ soal1.py
@@ -0,0 +1,15 @@
+print('='*40)
+print('*'*10, 'Justice league', '*'*10)
+#masukan nama
+def masukan_nama():
+print(" 1. brucewyne")
+print(" 2. Victorstone")
+print(" 3. ciscoramon")
+pil = int(input("Masukan username anda:"))
+if pil == 1:
+    print("'='*5, 'WELCOME vitorstone', '='*5 ")
+print('='*5, 'WELCOME vitorstone')
+print(" 1. Tambah Anggota Justice League")
+print(" 2. Hapus Anggota JUstice League")
+print(" 3. Tampilkan Anggota Justice league")
+print(" 4. Exit ")
